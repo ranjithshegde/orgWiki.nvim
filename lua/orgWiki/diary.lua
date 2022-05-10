1e64a028a2c4b6c996490f951132f8d4e93dc34a
ranjithshegde <ranjithshegde@gmail.com>
2022-05-11 01:06:54 +0200

    Format with stylua

--- lua/orgWiki/diary.lua
+++ lua/orgWiki/diary.lua
@@ -12,149 +12,149 @@ local diaryHeader = [[#+:TITLE: OrgWiki Diary index file
 local date
 
 local weeks = {
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
 }
 
 local months = {
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 }
 
 local getDate = function(day)
-    if day == -1 then
-        date = os.date("!*t", os.time() - 86400)
-    elseif day == 1 then
-        date = os.date("!*t", os.time() + 86400)
-    elseif day == 0 then
-        date = os.date "!*t"
-    end
-    if #tostring(date["day"]) == 1 then
-        date["day"] = 0 .. date["day"]
-    end
-    if #tostring(date["month"]) == 1 then
-        date["month"] = 0 .. date["month"]
-    end
+  if day == -1 then
+    date = os.date("!*t", os.time() - 86400)
+  elseif day == 1 then
+    date = os.date("!*t", os.time() + 86400)
+  elseif day == 0 then
+    date = os.date "!*t"
+  end
+  if #tostring(date["day"]) == 1 then
+    date["day"] = 0 .. date["day"]
+  end
+  if #tostring(date["month"]) == 1 then
+    date["month"] = 0 .. date["month"]
+  end
 
-    return string.format("%s-%s-%s %s.org", date["year"], date["month"], date["day"], weeks[date["wday"]])
+  return string.format("%s-%s-%s %s.org", date["year"], date["month"], date["day"], weeks[date["wday"]])
 end
 
 local getDiaryfiles = function()
-    local dirs = vim.fn.system "ls ~/Documents/Orgs/diary"
-    local list = vim.split(dirs, "\n")
-    local result = {}
+  local dirs = vim.fn.system "ls ~/Documents/Orgs/diary"
+  local list = vim.split(dirs, "\n")
+  local result = {}
 
-    for _, data in ipairs(list) do
-        if not string.find(data, "index") then
-            if data ~= "" then
-                table.insert(result, string.format("+ [[%s][%s]]", data, vim.fn.fnamemodify(tostring(data), ":r")))
-            end
-        end
+  for _, data in ipairs(list) do
+    if not string.find(data, "index") then
+      if data ~= "" then
+        table.insert(result, string.format("+ [[%s][%s]]", data, vim.fn.fnamemodify(tostring(data), ":r")))
+      end
     end
-    return result
+  end
+  return result
 end
 
 local insertDiarySep = function(list)
-    for index, value in ipairs(list) do
-        if type(value) == "string" then
-            if index == 1 then
-                local year = string.gsub(string.match(value, "%d*-"), "-", "")
-                local curMonth = string.match(value, "-%d*-")
-                curMonth = string.gsub(curMonth, "-", "")
-                curMonth = tonumber(curMonth)
-                curMonth = months[curMonth]
-                table.insert(list, index, string.format("*** *%s*\n", curMonth))
-                table.insert(list, index, string.format("** *%s*\n", year))
+  for index, value in ipairs(list) do
+    if type(value) == "string" then
+      if index == 1 then
+        local year = string.gsub(string.match(value, "%d*-"), "-", "")
+        local curMonth = string.match(value, "-%d*-")
+        curMonth = string.gsub(curMonth, "-", "")
+        curMonth = tonumber(curMonth)
+        curMonth = months[curMonth]
+        table.insert(list, index, string.format("*** *%s*\n", curMonth))
+        table.insert(list, index, string.format("** *%s*\n", year))
+      end
+      local previous = list[index - 1]
+      if type(previous) == "string" then
+        local curMonth = string.match(value, "-%d*-")
+        local prevMonth = string.match(previous, "-%d*-")
+        if curMonth and prevMonth then
+          curMonth = string.gsub(curMonth, "-", "")
+          prevMonth = string.gsub(prevMonth, "-", "")
+          prevMonth = tonumber(prevMonth)
+          curMonth = tonumber(curMonth)
+          if curMonth - prevMonth ~= 0 then
+            if curMonth - prevMonth <= 0 then
+              local year = string.gsub(string.match(value, "%d*-"), "-", "")
+              curMonth = months[curMonth]
+              table.insert(list, index, string.format("*** *%s*\n", curMonth))
+              table.insert(list, index, string.format("\n** *%s*\n", year))
+            else
+              curMonth = months[curMonth]
+              table.insert(list, index, string.format("\n*** *%s*\n", curMonth))
             end
-            local previous = list[index - 1]
-            if type(previous) == "string" then
-                local curMonth = string.match(value, "-%d*-")
-                local prevMonth = string.match(previous, "-%d*-")
-                if curMonth and prevMonth then
-                    curMonth = string.gsub(curMonth, "-", "")
-                    prevMonth = string.gsub(prevMonth, "-", "")
-                    prevMonth = tonumber(prevMonth)
-                    curMonth = tonumber(curMonth)
-                    if curMonth - prevMonth ~= 0 then
-                        if curMonth - prevMonth <= 0 then
-                            local year = string.gsub(string.match(value, "%d*-"), "-", "")
-                            curMonth = months[curMonth]
-                            table.insert(list, index, string.format("*** *%s*\n", curMonth))
-                            table.insert(list, index, string.format("\n** *%s*\n", year))
-                        else
-                            curMonth = months[curMonth]
-                            table.insert(list, index, string.format("\n*** *%s*\n", curMonth))
-                        end
-                    end
-                end
-            end
+          end
         end
+      end
     end
+  end
 end
 
 ---This function scans the diary_path for all files and creates or updates an index file
 ---The index is categorized into months and years automatically
 function diary.diaryGenerateIndex()
-    local files = getDiaryfiles()
-    insertDiarySep(files)
-    local output = io.open(diaryIndex, "w+")
-    output:write(diaryHeader)
-    for _, file in ipairs(files) do
-        file = file .. "\n"
-        output:write(file)
-    end
-    output:close()
-    pcall(vim.cmd, "redraw | e")
+  local files = getDiaryfiles()
+  insertDiarySep(files)
+  local output = io.open(diaryIndex, "w+")
+  output:write(diaryHeader)
+  for _, file in ipairs(files) do
+    file = file .. "\n"
+    output:write(file)
+  end
+  output:close()
+  pcall(vim.cmd, "redraw | e")
 end
 
 ---Create or open current day's diary entry
 ---@param editcmd string eg: "vs","e","tabnew"
 function diary.diaryTodayOpen(editcmd)
-    local name = getDate(0)
-    local opencmd = editcmd and editcmd .. " " or "e "
-    exec("cd " .. diaryPath)
-    vim.cmd(opencmd .. name)
+  local name = getDate(0)
+  local opencmd = editcmd and editcmd .. " " or "e "
+  exec("cd " .. diaryPath)
+  vim.cmd(opencmd .. name)
 end
 
 ---Open yesterday's diary entry
 ---@param editcmd string eg: "vs","e","tabnew"
 function diary.diaryYesterdayOpen(editcmd)
-    local name = getDate(-1)
-    local opencmd = editcmd and editcmd .. " " or "e "
-    exec("cd " .. diaryPath)
-    vim.cmd(opencmd .. name)
+  local name = getDate(-1)
+  local opencmd = editcmd and editcmd .. " " or "e "
+  exec("cd " .. diaryPath)
+  vim.cmd(opencmd .. name)
 end
 
 ---Create or open tomorrow's diary entry
 ---@param editcmd string eg: "vs","e","tabnew"
 function diary.diaryTomorrowOpen(editcmd)
-    local name = getDate(1)
-    local opencmd = editcmd and editcmd .. " " or "e "
-    exec("cd " .. diaryPath)
-    vim.cmd(opencmd .. name)
+  local name = getDate(1)
+  local opencmd = editcmd and editcmd .. " " or "e "
+  exec("cd " .. diaryPath)
+  vim.cmd(opencmd .. name)
 end
 
 ---Open diary index file
 ---@param editcmd string eg: "vs","e","tabnew"
 function diary.diaryIndexOpen(editcmd)
-    local opencmd = editcmd and editcmd .. " " or "e "
-    exec("cd " .. diaryPath)
-    vim.cmd(opencmd .. "index.org")
+  local opencmd = editcmd and editcmd .. " " or "e "
+  exec("cd " .. diaryPath)
+  vim.cmd(opencmd .. "index.org")
 end
 
 return diary
--- lua/orgWiki/init.lua
+++ lua/orgWiki/init.lua
@@ -1,95 +1,85 @@
 local orgwiki = {}
 
 local function set_keys(t)
-    local orgwiki_au = vim.api.nvim_create_augroup("OrgWiko", { clear = true })
+  local orgwiki_au = vim.api.nvim_create_augroup("OrgWiko", { clear = true })
 
-    vim.keymap.set("n", t.open_index, require("orgWiki.wiki").openIndex, { desc = "Open orgWiki Index file" })
-    vim.keymap.set("n", t.open_index_tab, function()
-        require("orgWiki.wiki").openIndex "tabnew"
-    end, { desc = "Open orgWiki Index in a new tab" })
+  vim.keymap.set("n", t.open_index, require("orgWiki.wiki").openIndex, { desc = "Open orgWiki Index file" })
+  vim.keymap.set("n", t.open_index_tab, function()
+    require("orgWiki.wiki").openIndex "tabnew"
+  end, { desc = "Open orgWiki Index in a new tab" })
 
-    vim.keymap.set("n", t.open_next_index, function()
-        require("orgWiki.wiki").nextWiki "tabnew"
-    end, { desc = "Open next available wiki a new tab" })
+  vim.keymap.set("n", t.open_next_index, function()
+    require("orgWiki.wiki").nextWiki "tabnew"
+  end, { desc = "Open next available wiki a new tab" })
 
-    vim.keymap.set("n", t.open_next_index, function()
-        require("orgWiki.wiki").select "tabnew"
-    end, { desc = "Open selected wiki a new tab" })
+  vim.keymap.set("n", t.open_next_index, function()
+    require("orgWiki.wiki").select "tabnew"
+  end, { desc = "Open selected wiki a new tab" })
 
-    vim.keymap.set(
+  vim.keymap.set(
+    "n",
+    t.open_diary_index,
+    require("orgWiki.diary").diaryIndexOpen,
+    { desc = "Open orgWiki Diary index file" }
+  )
+  vim.keymap.set("n", t.diary_today, require("orgWiki.diary").diaryTodayOpen, {
+    desc = "Open today's orgWiki Diary",
+  })
+  vim.keymap.set(
+    "n",
+    t.diary_tomorrow,
+    require("orgWiki.diary").diaryTomorrowOpen,
+    { desc = "Open tomorrow's orgWiki Diary" }
+  )
+  vim.keymap.set(
+    "n",
+    t.diary_yesterday,
+    require("orgWiki.diary").diaryYesterdayOpen,
+    { desc = "Open tomorrow's orgWiki Diary" }
+  )
+  vim.keymap.set(
+    "n",
+    t.diary_update,
+    require("orgWiki.diary").diaryGenerateIndex,
+    { desc = "Update orgWiki Diary index" }
+  )
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = orgwiki_au,
+    pattern = "org",
+    callback = function()
+      vim.keymap.set(
         "n",
-        t.open_diary_index,
-        require("orgWiki.diary").diaryIndexOpen,
-        { desc = "Open orgWiki Diary index file" }
-    )
-    vim.keymap.set("n", t.diary_today, require("orgWiki.diary").diaryTodayOpen, {
-        desc = "Open today's orgWiki Diary",
-    })
-    vim.keymap.set(
+        t.create_or_follow,
+        require("orgWiki.wiki").followOrCreate,
+        { buffer = true, desc = "create/follow link under cursor" }
+      )
+      vim.keymap.set("n", t.traverse_back, require("orgWiki.wiki").back, { buffer = true, desc = "Go to parent link" })
+      vim.keymap.set("n", t.goto_next, require("orgWiki.wiki").gotoNext, { buffer = true, desc = "Go to next link" })
+      vim.keymap.set(
         "n",
-        t.diary_tomorrow,
-        require("orgWiki.diary").diaryTomorrowOpen,
-        { desc = "Open tomorrow's orgWiki Diary" }
-    )
-    vim.keymap.set(
-        "n",
-        t.diary_yesterday,
-        require("orgWiki.diary").diaryYesterdayOpen,
-        { desc = "Open tomorrow's orgWiki Diary" }
-    )
-    vim.keymap.set(
-        "n",
-        t.diary_update,
-        require("orgWiki.diary").diaryGenerateIndex,
-        { desc = "Update orgWiki Diary index" }
-    )
-
-    vim.api.nvim_create_autocmd("FileType", {
-        group = orgwiki_au,
-        pattern = "org",
-        callback = function()
-            vim.keymap.set(
-                "n",
-                t.create_or_follow,
-                require("orgWiki.wiki").followOrCreate,
-                { buffer = true, desc = "create/follow link under cursor" }
-            )
-            vim.keymap.set(
-                "n",
-                t.traverse_back,
-                require("orgWiki.wiki").back,
-                { buffer = true, desc = "Go to parent link" }
-            )
-            vim.keymap.set(
-                "n",
-                t.goto_next,
-                require("orgWiki.wiki").gotoNext,
-                { buffer = true, desc = "Go to next link" }
-            )
-            vim.keymap.set(
-                "n",
-                t.goto_prev,
-                require("orgWiki.wiki").gotoPrev,
-                { buffer = true, desc = "Go to previous link" }
-            )
-        end,
-    })
+        t.goto_prev,
+        require("orgWiki.wiki").gotoPrev,
+        { buffer = true, desc = "Go to previous link" }
+      )
+    end,
+  })
 end
 
 local keys = {
-    create_or_follow = "<CR>",
-    traverse_back = "<BS>",
-    goto_next = "]w",
-    goto_prev = "[w",
-    open_index = "<leader>ww",
-    open_next_index = "<leader>wn",
-    open_choice_index = "<leader>wc",
-    open_index_tab = "<leader>wt",
-    open_diary_index = "<leader>wi",
-    diary_today = "<leader>w<leader>w",
-    diary_tomorrow = "<leader>w<leader>t",
-    diary_yesterday = "<leader>w<leader>y",
-    diary_update = "<leader>w<leader>i",
+  create_or_follow = "<CR>",
+  traverse_back = "<BS>",
+  goto_next = "]w",
+  goto_prev = "[w",
+  open_index = "<leader>ww",
+  open_next_index = "<leader>wn",
+  open_choice_index = "<leader>wc",
+  open_index_tab = "<leader>wt",
+  open_diary_index = "<leader>wi",
+  diary_today = "<leader>w<leader>w",
+  diary_tomorrow = "<leader>w<leader>t",
+  diary_yesterday = "<leader>w<leader>y",
+  diary_update = "<leader>w<leader>i",
 }
 
 ---This function initializes orgWiki by setting keymaps and defining path variables
@@ -112,30 +102,30 @@ local keys = {
 ---         diary_yesterday = "<leader>w<leader>y", -- Open yesterday's diary entry
 ---@param opts table
 function orgwiki.setup(opts)
-    assert(type(opts) == "table", "The setup function requires a table of arguments")
-    if opts.wiki_path then
-        vim.g.orgwiki_path = opts.wiki_path
-    else
-        print "Wiki path not set"
-    end
-    if opts.diary_path then
-        vim.g.orgwiki_diary_path = opts.diary_path
-    else
-        print "Diary path not set"
-    end
+  assert(type(opts) == "table", "The setup function requires a table of arguments")
+  if opts.wiki_path then
+    vim.g.orgwiki_path = opts.wiki_path
+  else
+    print "Wiki path not set"
+  end
+  if opts.diary_path then
+    vim.g.orgwiki_diary_path = opts.diary_path
+  else
+    print "Diary path not set"
+  end
 
-    if not opts.disable_mappings then
-        if opts.keys then
-            for key, _ in pairs(keys) do
-                if vim.fn.has_key(opts.keys, key) then
-                    keys[key] = opts.keys[key]
-                end
-            end
-            pcall(set_keys, keys)
-        else
-            set_keys(keys)
+  if not opts.disable_mappings then
+    if opts.keys then
+      for key, _ in pairs(keys) do
+        if vim.fn.has_key(opts.keys, key) then
+          keys[key] = opts.keys[key]
         end
+      end
+      pcall(set_keys, keys)
+    else
+      set_keys(keys)
     end
+  end
 end
 
 return orgwiki
